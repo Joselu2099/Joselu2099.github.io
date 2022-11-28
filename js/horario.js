0bde758429a2c293e0ca53759798f754373b8d7d
Isa <38717344+Joselu2099@users.noreply.github.com>
2022-11-28 14:25:50 +0100

    Creandos horarios + Header

---
+++ js/horario.js
@@ -0,0 +1,8 @@
+let horarioManana = "<tr><td>11:00-12:00</td></tr>";       
+
+let horarioTarde = "";   
+
+document.getElementById("horarioManana").innerHTML += horarioManana+"</table>";
+document.getElementById("horarioTarde").innerHTML += horarioTarde;
+
+alert("hola");
