75098aaab7732ac48274cccf01c6241464a570cf
Isa <38717344+Joselu2099@users.noreply.github.com>
2022-11-29 12:30:26 +0100

    Quitada función innecesaria

--- js/horario.js
+++ js/horario.js
@@ -1,11 +1,3 @@
-function init(){
-    let horarioManana = document.getElementById("horarioManana");
-    horarioManana.style.display = "none";
-    let horarioTarde = document.getElementById("horarioTarde");
-    horarioTarde.style.display = "none";
-    alert("hola");
-}
-
 function mostrarHorarioManana(){
     let horarioManana = document.getElementById("horarioManana");
     if (horarioManana.style.display === "none") {
